ccd4551b4580d5b7d4bb55d4e0ebb4efc091f5f0
Jeann1809 <carlojeanalmario@gmail.com>
2025-08-28 10:01:42 -0500

    -

--- helpers/emailHandler.js
+++ helpers/emailHandler.js
@@ -1,20 +1,26 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: true, // true for port 465, false for 587
+  service: 'gmail', // tells Nodemailer to use Gmail settings
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
+    user: process.env.EMAIL_USER, // your Gmail address
+    pass: process.env.EMAIL_PASS  // your Gmail App Password
   }
 });
 
 export async function sendEmail(to, subject, html) {
-  await transporter.sendMail({
-    from: process.env.SMTP_USER,
-    to,
-    subject,
-    html
-  });
+  try {
+    let info = await transporter.sendMail({
+      from: `"Marimar Crochet" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html
+    });
+    console.log("Email sent:", info.messageId);
+  } catch (err) {
+    console.error("Error while sending email:", err);
+  }
 }
